8759809d8e021abb98e82ecce9a47c94dcf9cc9e
remciszewski <remciszewski@icloud.com>
2024-06-04 22:31:15 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int len = 10;
+        if (args.Length > 0 && int.TryParse(args[0], out int parsedLen))
+        {
+            len = parsedLen;
+        }
+        GenerateFibonacci(len);
+    }
+
+    static void GenerateFibonacci(int len)
+    {
+        if (len < 1) return;
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        Console.Write("{0} {1}", a, b);
+        for (int i = 2; i < len; i++)
+        {
+            c = a + b;
+            Console.Write(" {0}", c);
+            a = b;
+            b = c;
+        }
+        Console.WriteLine();
+    }
+}
